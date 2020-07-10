b342c6219b74bfae51f620c6a666c19dc411f950
Vinoo Selvarajah <selv@rajah.dev>
2020-07-10 09:24:14 -0400

    Initial Commit

---
+++ plong/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  plong
+//
+//  Created by Vinoo Selvarajah on 7/10/20.
+//  Copyright © 2020 Vinoo Selvarajah. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
